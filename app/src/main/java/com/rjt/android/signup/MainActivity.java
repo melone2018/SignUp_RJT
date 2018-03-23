77f2d85689e460b1971ccc05df018abba9303f49
u0733159 <u0733159@tamu.edu>
2018-03-23 11:36:52 -0500

    Added OnItemSelectedListener,OnCheckedChangeListener and made Chinese and English Versions of the app

--- app/src/main/java/com/rjt/android/signup/MainActivity.java
+++ app/src/main/java/com/rjt/android/signup/MainActivity.java
@@ -7,25 +7,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener, AdapterView.OnItemSelectedListener {
     private static final int SIGN_UP_CODE = 7;
+    private CheckBox remPassword, newUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("onCreat", "onCreate started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialUISetup();
+    }
+
+    public void initialUISetup(){
         EditText nameEditText = findViewById(R.id.editTextName);
         nameEditText.setOnFocusChangeListener(this);
 
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setPrompt("select country");
         spinner.setOnItemSelectedListener(this);
-    }
 
+        remPassword = findViewById(R.id.checkBox);
+        newUser = findViewById(R.id.newUserCheckBox);
+
+        remPassword.setOnCheckedChangeListener(new signUpCheckBoxChangeListerner());
+        newUser.setOnCheckedChangeListener(new signUpCheckBoxChangeListerner());
+
+    }
     public void signUpHandler(View view) {
         Log.i("signUpHandler", "signUpHandler Started");
         EditText editTextName = findViewById(R.id.editTextName);
@@ -37,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnFocusChang
         startActivityForResult(signUpIntent, SIGN_UP_CODE);
     }
 
+    public void textNotification(String notificationText){
+        Toast.makeText(this, notificationText, Toast.LENGTH_SHORT).show();
+    }
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
         if(hasFocus){
@@ -57,5 +73,26 @@ public class MainActivity extends AppCompatActivity implements View.OnFocusChang
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    class signUpCheckBoxChangeListerner implements CheckBox.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                if (buttonView == remPassword) {
+                    textNotification("You checked \"Remember Password\"");
+                }
+                if (buttonView == newUser) {
+                    textNotification("You checked \"New User\"");
+                }
+            } else {
+                if (buttonView == remPassword) {
+                    textNotification("You unchecked \"Remember Password\"");
+                }
+                if (buttonView == newUser) {
+                    textNotification("You unchecked \"New User\"");
+                }
+            }
+        }
     }
 }
